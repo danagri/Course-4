f706d14d9bc7b0f12f272f70be9dc95dae1350bf
Danagri <bgrizuk@gmail.com>
2018-12-05 10:54:44 +0200

    First

--- node-express/routes/leaderRouter.js
+++ node-express/routes/leaderRouter.js
@@ -5,40 +5,42 @@ const leaderRouter = express.Router();
 leaderRouter.use(bodyParser.json());
 
 leaderRouter.route('/')
-.all((req, res, next) => {
+.all((req,res,next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     next();
 })
-.get((req, res, next) => {
+.get((req,res,next) => {
     res.end('Will send all the leaders to you!');
 })
 .post((req, res, next) => {
-    res.end('Will add the leader: ' + req.body.name + 
-        ' with details ' + req.body.description);
+    res.end('Will add the leader: ' + req.body.name + ' with details: ' + req.body.description);
 })
 .put((req, res, next) => {
     res.statusCode = 403;
-    res.end('Put operation not supported on /leaders');
+    res.end('PUT operation not supported on /leaders');
 })
 .delete((req, res, next) => {
-    res.end('Deleting all the leaders!');
+    res.end('Deleting all leaders');
 });
 
 leaderRouter.route('/:leaderId')
-.get((req, res, next) => {
-    res.end('Will send details of the leader: ' 
-        + req.params.leaderId  + ' to you!');
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send details of the leader: ' + req.params.leaderId +' to you!');
 })
 .post((req, res, next) => {
-    res.statusCode = 403;
-    res.end('Post operation not supported on /leaders/' + 
-        req.params.leaderId);
+  res.statusCode = 403;
+  res.end('POST operation not supported on /leaders/'+ req.params.leaderId);
 })
 .put((req, res, next) => {
-    res.write('Updating the leader: ' + req.params.leaderId + '\n');
-    res.end('Will update the leader: ' + req.body.name +
-        ' with details ' + req.body.description);
+  res.write('Updating the leader: ' + req.params.leaderId + '\n');
+  res.end('Will update the leader: ' + req.body.name + 
+        ' with details: ' + req.body.description);
 })
 .delete((req, res, next) => {
     res.end('Deleting leader: ' + req.params.leaderId);
